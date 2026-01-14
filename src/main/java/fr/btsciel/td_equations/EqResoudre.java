e8d84984448f618f689d6e830174e46b8ff9d44b
Mouton Eloane <eloane.mouton@lyceeastier.com>
2026-01-14 16:15:57 +0100

    save

--- src/main/java/fr/btsciel/td_equations/EqResoudre.java
+++ src/main/java/fr/btsciel/td_equations/EqResoudre.java
@@ -6,6 +6,7 @@ public class EqResoudre {
 
     private String pattern = "([-+]?[0-9][xX]? [-+/*] [0-9] [=] ([0-9]|[0-9][0-9]))";
     public String  resoudre (String equation) {
+
         return "Ok";
     }
     public boolean typeEquationOk (String equation) {
